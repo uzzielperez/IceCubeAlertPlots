2c6a3ab90af29d26829622235234512a21f2ff22
Uzziel Perez <cuperez@fnal.gov>
2018-03-30 00:15:42 -0500

    additional catalogs

--- TempScripts/Plot.py
+++ TempScripts/Plot.py
@@ -63,4 +63,60 @@ srcs_2fhl = getCatPositions(fermiCats.get('2FHL'))
 ax.scatter(srcs_2fhl[0], srcs_2fhl[1], color='orange', marker='x', label='2FHL', s=100)
 
 # Plot 3FGL sources
-srcs_3fgl = getCatPositions()
+srcs_3fgl = getCatPositions(fermiCats.get('3FGL'))
+ax.scatter(srcs_3fgl[0], srcs_3fgl[1], color='none', edgecolor='blue', s=300, label='3FGL', linewidth=2)
+
+for s in np.array(srcs_3fgl).T:
+    if openingAngle(float(s[0]), float(s[1]), RA0, Dec0) < Box:
+        ax.annotate(s[2], (float(s[0])-0.3, float(s[1])), color='blue', horizontalalignment='left', verticalalignment='center', fontsize=12)
+
+# Read BZCat
+bzcat = np.genfromtxt("/Users/up/Desktop/IceCube/Fermi-Neutrino alerts/5bzu.dat", delimiter=',', dtype=None, names=True)
+
+# Parse BZCat strings
+def procRA(s):
+    #print(s)
+    l = str(s).split(' ')
+    ra = 15 * (float(l[1]) + float(l[2])/60. + float(l[3])/3600.)
+    return ra 
+
+def procDec(s):
+    #print(s)
+    l = str(s).split(' ')
+    dec = np.abs(float(l[1])) + float(l[2])/60. + float(l[3])/3600.
+
+    if '-' in l[1]:
+        return -dec
+    else:
+        return dec
+ra_bzcat = [procRA(s) for s in bzcat['RA']]
+dec_bzcat = [procDec(s) for s in bzcat['Dec']]
+
+ax.scatter(ra_bzcat, dec_bzcat, color='#666666', label='BZCat')
+
+for s in bzcat:
+    ra = procRA(s['RA'])
+    dec = procDec(s['Dec'])
+    name = str(s['Source_Name']).split('\'')
+    if openingAngle(ra, dec, RA0, Dec0) < Box: 
+        ax.annotate(name[1], (ra-0.1, dec+0.3), color='#666666', horizontalalignment='left', verticalalignment='center', fontsize=12, clip_on=True)
+
+# Plot neutrino position
+ax.scatter(RA0, Dec0, color='crimson', marker='x', label='IC170922 - GCN', s=100)
+xe = RA0 + (dRAhi - dRAlo)/2.
+ye = Dec0 + (dDechi - dDeclo)/2.
+e = Ellipse(xy=(xe,ye), width=dRAhi+dRAlo, height=dDechi+dDeclo, linewidth=2, edgecolor='crimson', facecolor='none')
+ax.add_artist(e)
+e.set_alpha(1.)
+
+# Legend outside the plot
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+# Limits, labels and grid
+ax.set_xlim(RA0+Box, RA0-Box)
+ax.set_ylim(Dec0-Box, Dec0+Box)
+ax.set_xlabel(r'$\alpha_{2000}$ [$^{\circ}$]')
+ax.set_ylabel(r'$\delta_{2000}$ [$^{\circ}$]')
+ax.grid(linestyle='--', linewidth=0.5)
