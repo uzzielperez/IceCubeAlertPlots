59e3569d5d96d7b08e8864f1c3f68f8899f9a2d0
Uzziel Perez <cuperez@fnal.gov>
2018-04-12 23:21:14 -0500

    added master runner

--- TempScripts/GCNlistener.py
+++ TempScripts/GCNlistener.py
@@ -1,7 +1,8 @@
 import gcn
 import sys
-import subprocess
+import subprocess as sp
 from amonParser import loadAMON
+
 #------------- Listen
 # See syntax for pygcn listen
 #------------- Alert
@@ -28,21 +29,21 @@ from amonParser import loadAMON
 #  -b BOX, --Box BOX     Box
 #########################################################################
 
-#-------------- 
-#```bash 
-#  
-#  condapython Plot.py -ra RASC -rh  DRAHI -rl DRALO -d DEC -dh DDECHI -dl DDECLO -b BOX 
-#
-#```
-## Goes like  
-## condapython is an alias to miniconda python3
-#--------------
-# GCN alert delivers amonfile 
-RASC, DRAHI, DRALO, DEC, DDECHI, DDECLO, BOX = loadAMON(amonfile) 
+# GCN alert delivers amonfile. We parse it with amonParser.py. Call loadAMON(amonfile) to
+# Test #
+amonfile = '17569642_130214.amon'
+#amonfile = '34032434_130171.amon' 
 
-bashcmd = "/path/to/python Plot.py  -ra RASC -rh  DRAHI -rl DRALO -d DEC -dh DDECHI -dl DDECLO -b BOX"
-process = subproces.Popen(bashcmd.split(), stdout=subprocess.PIPE)
+RASC, DRAHI, DRALO, DEC, DDECHI, DDECLO, BOX, EVNUM, RUN = loadAMON(amonfile) 
+
+# When loadAMON is called, we run Plot.py and supply arguments
+# python Plot.py  -ra RASC -rh  DRAHI -rl DRALO -d DEC -dh DDECHI -dl DDECLO -b BOX"
+bashcmd = 'python Plot.py -ra %d -rh %d -rl %d -d %d -dh %d -dl %d -g %s_%s' %(RASC, DRAHI, DRALO, DEC, DDECHI, DDECLO, EVNUM, RUN) # leave out box in dev stage
+process = sp.Popen(bashcmd.split(), stdout=sp.PIPE)
 output, error = process.communicate()
+#status = sp.Popen.poll(process) # status should be 'None'
+#sp.Popen.terminate(process) #closes the process
+#status = sp.Popen.poll(process) # status not 'None'
 #--------------
 
 # end Plot.py. Continuously listen to GCN
